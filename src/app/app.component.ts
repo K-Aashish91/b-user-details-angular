3c1401d1d4163b80aa450f9af9c0080a0dbaa106
K.Aashish <105279792+K-Aashish91@users.noreply.github.com>
2022-09-04 02:45:15 -0700

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'b-user-details-angular';
+  name='siddhesh Kumbhar';
+  address='Mumbai';
+  contact='9673805618';
+}
